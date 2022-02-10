35940297c4cd3e9d881c64e9b69d49d2d10b0795
Yaroslav <y.suvorov@yadro.com>
2022-02-11 00:27:34 +0300

    Fix variable names

--- draw_table.py
+++ draw_table.py
@@ -2,15 +2,15 @@ from terminaltables import DoubleTable
 
 
 def prepare_table(statistic_of_languages: dict) -> list:
-    basic_table_data = [
+    table_data = [
         ['Язык программирования', 'Вакансий найдено', 'Вакансий обработано', 'Средняя зарплата'],
     ]
-    for language_name, statistics in statistic_of_languages.items():
-        data = [language_name]
-        for values in statistics.values():
-            data.append(values)
-        basic_table_data.append(data)
-    return basic_table_data
+    for language, statistics in statistic_of_languages.items():
+        temp_data = [language]
+        for value in statistics.values():
+            temp_data.append(value)
+        table_data.append(temp_data)
+    return table_data
 
 
 def draw_table(title: str, statistic_of_languages: dict):
