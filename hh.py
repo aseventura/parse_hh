b3018f64b2a256f569666f4add119f6d31c27bbe
Yaroslav <y.suvorov@yadro.com>
2022-02-11 00:34:44 +0300

    Change main logic

--- hh.py
+++ hh.py
@@ -5,18 +5,13 @@ from predict_salary import predict_salary
 def predict_rub_salary_for_hh(vacancy_info: dict):
     if vacancy_info['salary'] and vacancy_info['salary']['currency'] == 'RUR':
         return predict_salary(vacancy_info['salary']['from'], vacancy_info['salary']['to'])
-    return
 
 
 def parse_hh_vacancies(programming_languages: tuple) -> dict:
     base_url = 'https://api.hh.ru/vacancies'
-    search_requests = (
-        'Python Django NOT go NOT golang', 'Java Spring', 'C++ Boost', 'PHP Symphony',
-        'JavaScript React NOT Angular', 'Golang NOT python', 'Swift iOS', 'Ruby Rails',
-    )
     statistic_of_languages = {}
 
-    for i, language in enumerate(programming_languages):
+    for language in programming_languages:
         language_info = {
             'vacancies_found': 0,
             'vacancies_processed': 0,
@@ -25,22 +20,22 @@ def parse_hh_vacancies(programming_languages: tuple) -> dict:
         query_string = {
             'area': '1',                    # Moscow location
             'period': '30',                 # Last 30 days
-            'text': search_requests[i],     # Search field
+            'text': language,               # Search field
             'page': 0,                      # Start page
         }
-        response = requests.get(base_url, params=query_string)
-        response.raise_for_status()
-        language_info['vacancies_found'] = response.json()['found']
-        search_results = response.json()['items']
-
-        while query_string['page'] < response.json()['pages']:
-            query_string['page'] += 1
+        hh_pages = 1
+        all_vacancies = []
+        while query_string['page'] < hh_pages:
             response = requests.get(base_url, params=query_string)
             response.raise_for_status()
-            search_results += response.json()['items']
+            hh_response = response.json()
+            language_info['vacancies_found'] = hh_response['found']
+            all_vacancies += hh_response['items']
+            hh_pages = hh_response['pages']
+            query_string['page'] += 1
 
-        for vacancy_info in search_results:
-            job_salary = predict_rub_salary_for_hh(vacancy_info)
+        for vacancy in all_vacancies:
+            job_salary = predict_rub_salary_for_hh(vacancy)
             if job_salary:
                 language_info['vacancies_processed'] += 1
                 language_info['average_salary'] += job_salary
